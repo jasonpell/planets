a778834dd86f8c53f47e4d7926ba175831e891de
Jason Pell <jason.pell@gmail.com>
2017-02-04 16:41:19 -0500

    modify analysis script to load csv file into sqlite

--- analyze.py
+++ analyze.py
@@ -1,7 +1,19 @@
 import sqlite3
-connection = sqlite3.connect("survey.db")
+import csv
+
+connection = sqlite3.connect(":memory:")
 cursor = connection.cursor()
-cursor.execute("SELECT Site.lat, Site.long FROM Site;")
+cursor.execute("CREATE TABLE t (name,age);")
+
+with open('data.csv', 'r') as fin:
+  dr = csv.DictReader(fin)
+  to_db = [(i['name'], i['age']) for i in dr]
+
+cursor.executemany("INSERT INTO t (name,age) VALUES (?, ?);", to_db)
+connection.commit()
+
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM t;")
 results = cursor.fetchall()
 for r in results:
  print(r)
